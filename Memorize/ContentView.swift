f29041e31eb78c8f40c4aa5340f37602e1329fd1
AnthonyBuilder <anthonyasusxbox2014@gmail.com>
2021-03-30 16:46:49 -0300

    Finished Lecture: 1 Logics and Introduction

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,14 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello")
+        return HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }.padding()
+        .font(.largeTitle)
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 25.0)
+                    .stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 25.0).fill()
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
